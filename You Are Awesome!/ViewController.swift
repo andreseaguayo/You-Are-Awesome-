044ea45a9f826eccc2167fbf988737c2e9cf6213
Andres Aguayo <andresaguayo@ymail.com>
2021-11-28 16:49:29 -0800

    added button

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("view did load has run! 😜")
+        messageLabel.text = "What is this?"
     }
 
 
+    @IBAction func addNotePressed(_ sender: UIButton) {
+        print("The message button was pressed!😎")
+        messageLabel.text = "You are awesome!"
+    }
 }
 
